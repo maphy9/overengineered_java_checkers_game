73e299bd81051fa658176e052e3d6125e9b3644e
maphy9 <maphy9@gmail.com>
2025-05-07 00:24:00 +0200

    Fix check getLongestAttackSequences

--- src/main/java/org/pieces/pieces/Man.java
+++ src/main/java/org/pieces/pieces/Man.java
@@ -38,18 +38,18 @@ public abstract class Man extends Piece {
         int col = getCol() - 1;
 
         List<AttackSequence> attackSequences = new ArrayList<>();
-        if (targetRow < BOARD_SIZE && targetCol < BOARD_SIZE && targetRow > 0 && targetCol > 0) {
-            if (board[targetRow][targetCol] == ' ' && board[captureRow][captureRow] == 'E') {
+        if (targetRow < BOARD_SIZE && targetCol < BOARD_SIZE && targetRow >= 0 && targetCol >= 0) {
+            if (board[targetRow][targetCol] == ' ' && board[captureRow][captureCol] == 'E') {
                 board[targetRow][targetCol] = 'P';
                 board[captureRow][captureCol] = ' ';
                 board[row][col] = ' ';
-                setRow(row + 3);
-                setCol(col + 3);
+                setRow(targetRow + 1);
+                setCol(targetCol + 1);
                 List<AttackSequence> nextSequences = getLongestAttackSequences(board);
                 setRow(row + 1);
                 setCol(col + 1);
-                board[row + 2][col + 2] = ' ';
-                board[row + 1][col + 1] = 'E';
+                board[targetRow][targetCol] = ' ';
+                board[captureRow][captureCol] = 'E';
                 board[row][col] = 'P';
 
                 if (nextSequences.isEmpty()) {
@@ -62,8 +62,8 @@ public abstract class Man extends Piece {
                 } else {
                     attackSequences = nextSequences;
                     for (AttackSequence attackSequence : nextSequences) {
-                        attackSequence.sourceRow = getRow();
-                        attackSequence.sourceCol = getCol();
+                        attackSequence.sourceRow = row + 1;
+                        attackSequence.sourceCol = col + 1;
                         attackSequence.length += 1;
                         attackSequence.capturedPositions.addFirst(new int[] { captureRow + 1, captureCol + 1 });
                     }
@@ -79,141 +79,14 @@ public abstract class Man extends Piece {
         int row = getRow() - 1;
         int col = getCol() - 1;
 
-        List<AttackSequence> attackBottomRight = getLongestAttackSequencesHelper(board, row + 2, col + 2, row + 1, col + 1);
-        if (row + 2 < BOARD_SIZE && col + 2 < BOARD_SIZE) {
-            if (board[row + 2][col + 2] == ' ' && board[row + 1][col + 1] == 'E') {
-                board[row + 2][col + 2] = 'P';
-                board[row + 1][col + 1] = ' ';
-                board[row][col] = ' ';
-                setRow(row + 3);
-                setCol(col + 3);
-                List<AttackSequence> nextSequences = getLongestAttackSequences(board);
-                setRow(row + 1);
-                setCol(col + 1);
-                board[row + 2][col + 2] = ' ';
-                board[row + 1][col + 1] = 'E';
-                board[row][col] = 'P';
-
-                if (nextSequences.isEmpty()) {
-                    AttackSequence tmp = new AttackSequence(this);
-                    attackBottomRight.add(tmp);
-                    tmp.targetRow = getRow() + 2;
-                    tmp.targetCol = getCol() + 2;
-                    tmp.length = 1;
-                    tmp.capturedPositions.add(new int[] { getRow() + 1, getCol() + 1 });
-                } else {
-                    attackBottomRight = nextSequences;
-                    for (AttackSequence attackSequence : nextSequences) {
-                        attackSequence.sourceRow = getRow();
-                        attackSequence.sourceCol = getCol();
-                        attackSequence.length += 1;
-                        attackSequence.capturedPositions.addFirst(new int[] { getRow() + 1, getCol() + 1 });
-                    }
-                }
-            }
-        }
-
-        List<AttackSequence> attackBottomLeft = new ArrayList<>();
-        if (row + 2 < BOARD_SIZE && col - 2 >= 0) {
-            if (board[row + 2][col - 2] == ' ' && board[row + 1][col - 1] == 'E') {
-                board[row + 2][col - 2] = 'P';
-                board[row + 1][col - 1] = ' ';
-                board[row][col] = ' ';
-                setRow(row + 3);
-                setCol(col - 1);
-                List<AttackSequence> nextSequences = getLongestAttackSequences(board);
-                setRow(row + 1);
-                setCol(col + 1);
-                board[row + 2][col - 2] = ' ';
-                board[row + 1][col - 1] = 'E';
-                board[row][col] = 'P';
-
-                if (nextSequences.isEmpty()) {
-                    AttackSequence tmp = new AttackSequence(this);
-                    attackBottomLeft.add(tmp);
-                    tmp.targetRow = getRow() + 2;
-                    tmp.targetCol = getCol() - 2;
-                    tmp.length = 1;
-                    tmp.capturedPositions.add(new int[] { getRow() + 1, getCol() - 1 });
-                } else {
-                    attackBottomLeft = nextSequences;
-                    for (AttackSequence attackSequence : nextSequences) {
-                        attackSequence.sourceRow = getRow();
-                        attackSequence.sourceCol = getCol();
-                        attackSequence.length += 1;
-                        attackSequence.capturedPositions.addFirst(new int[] { getRow() + 1, getCol() - 1 });
-                    }
-                }
-            }
-        }
-
-        List<AttackSequence> attackTopRight = new ArrayList<>();
-        if (row - 2 >= 0 && col + 2 < BOARD_SIZE) {
-            if (board[row - 2][col + 2] == ' ' && board[row - 1][col + 1] == 'E') {
-                board[row - 2][col + 2] = 'P';
-                board[row - 1][col + 1] = ' ';
-                board[row][col] = ' ';
-                setRow(row - 1);
-                setCol(col + 3);
-                List<AttackSequence> nextSequences = getLongestAttackSequences(board);
-                setRow(row + 1);
-                setCol(col + 1);
-                board[row - 2][col + 2] = ' ';
-                board[row - 1][col + 1] = 'E';
-                board[row][col] = 'P';
-
-                if (nextSequences.isEmpty()) {
-                    AttackSequence tmp = new AttackSequence(this);
-                    attackTopRight.add(tmp);
-                    tmp.targetRow = getRow() - 2;
-                    tmp.targetCol = getCol() + 2;
-                    tmp.length = 1;
-                    tmp.capturedPositions.add(new int[] { getRow() - 1, getCol() + 1 });
-                } else {
-                    attackTopRight = nextSequences;
-                    for (AttackSequence attackSequence : nextSequences) {
-                        attackSequence.sourceRow = getRow();
-                        attackSequence.sourceCol = getCol();
-                        attackSequence.length += 1;
-                        attackSequence.capturedPositions.addFirst(new int[] { getRow() - 1, getCol() + 1 });
-                    }
-                }
-            }
-        }
-
-        List<AttackSequence> attackTopLeft = new ArrayList<>();
-        if (row - 2 >= 0 && col - 2 >= 0) {
-            if (board[row - 2][col - 2] == ' ' && board[row - 1][col - 1] == 'E') {
-                board[row - 2][col - 2] = 'P';
-                board[row - 1][col - 1] = ' ';
-                board[row][col] = ' ';
-                setRow(row - 1);
-                setCol(col - 1);
-                List<AttackSequence> nextSequences = getLongestAttackSequences(board);
-                setRow(row + 1);
-                setCol(col + 1);
-                board[row - 2][col - 2] = ' ';
-                board[row - 1][col - 1] = 'E';
-                board[row][col] = 'P';
-
-                if (nextSequences.isEmpty()) {
-                    AttackSequence tmp = new AttackSequence(this);
-                    attackTopLeft.add(tmp);
-                    tmp.targetRow = getRow() - 2;
-                    tmp.targetCol = getCol() - 2;
-                    tmp.length = 1;
-                    tmp.capturedPositions.add(new int[] { getRow() - 1, getCol() - 1 });
-                } else {
-                    attackTopLeft = nextSequences;
-                    for (AttackSequence attackSequence : nextSequences) {
-                        attackSequence.sourceRow = getRow();
-                        attackSequence.sourceCol = getCol();
-                        attackSequence.length += 1;
-                        attackSequence.capturedPositions.addFirst(new int[] { getRow() - 1, getCol() - 1 });
-                    }
-                }
-            }
-        }
+        List<AttackSequence> attackBottomRight
+            = getLongestAttackSequencesHelper(board, row + 2, col + 2, row + 1, col + 1);
+        List<AttackSequence> attackBottomLeft
+            = getLongestAttackSequencesHelper(board, row + 2, col - 2, row + 1, col - 1);
+        List<AttackSequence> attackTopRight
+            = getLongestAttackSequencesHelper(board, row - 2, col + 2, row - 1, col + 1);
+        List<AttackSequence> attackTopLeft
+            = getLongestAttackSequencesHelper(board, row - 2, col - 2, row - 1, col - 1);
 
         List<AttackSequence> longestAttackSequence = attackBottomRight;
 
